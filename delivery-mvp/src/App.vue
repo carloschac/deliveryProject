4883cecd0051c77d5d85a13d2f3507a89e51dd46
Carlos Henrique <carloss.chac@gmail.com>
2023-07-26 12:01:07 -0300

    commit

---
+++ delivery-mvp/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <navBar />
+    <!-- Use o componente navBar no template -->
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+import navBar from './components/navBar.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    navBar,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
